5dd66f5a64ef1045492de4685772b636e3d9049c
canersonerseker <canerseker444@gmail.com>
2026-06-27 15:59:15 +0300

    Add Todo gRPC service with EF Core and remove Greeter
    Added todo.proto and EF Core (InMemory) integration. Registered AppDbContext, created TodoItem entity, and removed GreeterService. Fixed BulkCreateResponse field naming in todo.proto. Updated project references and structure.

--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -1,18 +1,21 @@
-using GrpcServer.Services;
+using GrpcServer.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-
 // Add services to the container.
+
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+               opt.UseInMemoryDatabase("TodoDb"));
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
